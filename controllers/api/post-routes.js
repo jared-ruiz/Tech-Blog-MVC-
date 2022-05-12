50fc8998dd10c511a386c2f8096af62b266d7401
Jared Ruiz <jared707@gmail.com>
2022-05-11 23:25:53 -0500

    Added all post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,111 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+//GET all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        //order of created at property AND in decending order
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                //include User info since they are related models
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+//GET single post by id, includes user model along with username 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with that id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    }) 
+})
+
+//POST create new post
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+//PUT update post at specific id
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with that id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+//DELETE delete post at specific id
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with that id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,4 +2,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//model associations 
+User.hasMany(Post, {
+    //id column in user conencts to user_id in post
+    foreignKey: 'user_id'
+})
+
+//reverse association for above
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 module.exports = { User, Post };
