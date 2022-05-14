60cf8f0effef78661890f0933b18b345a0282524
Jared Ruiz <jared707@gmail.com>
2022-05-14 14:50:25 -0500

    Update login route upon successful login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -21,7 +21,8 @@ async function signupFormHandler(event) {
         
         //check the repsonse status
         if (response.ok) {
-            console.log('success')
+            console.log('success');
+            document.location.replace('/dashboard');
         }
         else {
             alert(response.statusText);
